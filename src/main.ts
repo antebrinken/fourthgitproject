d5e6c2fc5d8877a09608bbbb207a5005f58cde3f
Madiha <madihaiftikhar2013@gmail.com>
2024-12-09 15:06:57 +0100

    code

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,7 @@ import '.src/style.css'
 
     const name = "Amanda"
     console.log(name.length);
+    const firstName = document.createElement("p");
+    firstName.innerHTML="madiha";
+    const div = document.getElementById("app");
+    div?.appendChild(firstName);
